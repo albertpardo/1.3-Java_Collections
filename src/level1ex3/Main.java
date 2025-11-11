48b2587648c9524119d1bce6c48fe3485b76b893
apm <apm@mail.com>
2025-11-11 13:52:09 +0100

    [T3L1E3] refactor test read countries.txt

--- src/level1ex3/Main.java
+++ src/level1ex3/Main.java
@@ -8,17 +8,27 @@ public class Main {
 
     }
 
+    private static void printReadFileTxtContent(InputStream inputStream){
+        Scanner sc = new Scanner(inputStream);
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
+    }
+
+    private static boolean existFile(InputStream file){
+        if (file != null)
+            return true;
+        System.err.println(FilesNamesAsConstants.COUNTRIES + " has not been found.");
+        return false;
+    }
+
     public static void main(String[] args) {
 
         InputStream inputStream = Main.class.getResourceAsStream(FilesNamesAsConstants.COUNTRIES);
-        if (inputStream == null) {
-            System.err.println(FilesNamesAsConstants.COUNTRIES + " has not been found.");
-        } else {
-            Scanner sc = new Scanner(inputStream);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
-            }
-            sc.close();
+
+        if (existFile(inputStream)){
+            printReadFileTxtContent(inputStream);
         }
     }
 }
